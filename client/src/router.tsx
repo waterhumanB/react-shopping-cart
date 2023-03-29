047f891967b69f86dabda01f48f8c57bfb871083
waterhumanB <dpfflsk45@gmail.com>
2023-03-29 20:32:36 +0900

    refactor: router 데이터 수정

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,28 +5,20 @@ import ProductList from './products/pages/ProductList'
 import ShoppingCart from './shoppingCart/pages/ShoppingCart'
 
 interface RouterBase {
-  id: number
   path: string
-  label: string
   element: React.ReactNode
 }
 
 const routerData: RouterBase[] = [
   {
-    id: 0,
-    label: '상품목록록',
     path: '/',
     element: <ProductList />,
   },
   {
-    id: 1,
-    label: '장바구니',
     path: '/shopping-cart',
     element: <ShoppingCart />,
   },
   {
-    id: 2,
-    label: '주문목록',
     path: '/order-list',
     element: <OrderList />,
   },
